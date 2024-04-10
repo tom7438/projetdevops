e095890ecd6760dcb77c34b3cf660c76e4bd3b24
4ppolyon <romalves70@gmail.com>
2024-04-10 09:53:44 +0200

    Modif BeforeEach

--- src/test/java/DataFrameTest.java
+++ src/test/java/DataFrameTest.java
@@ -1,3 +1,5 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -7,7 +9,11 @@ import java.util.LinkedHashMap;
 import java.util.List;
 
 public class DataFrameTest {
-    private DataFrame createTestDataFrame() {
+    static DataFrame df;
+    static DataFrame df2;
+    @BeforeEach
+    public void testDataFrameCreationList() {
+        System.out.println("_________________Création d'un DataFrame de test_________________");
         List<List<Object>> data = new ArrayList<>(Arrays.asList(
                 new ArrayList<>(Arrays.asList(1, "Bonjour", true)),
                 new ArrayList<>(Arrays.asList(2, "Monde", false))
@@ -16,10 +22,13 @@ public class DataFrameTest {
         columns.put("A", Integer.class);
         columns.put("B", String.class);
         columns.put("C", Boolean.class);
-        return new DataFrame(data, columns);
+        df = new DataFrame(data, columns);
+        assertNotEquals(null, df);
     }
 
-    private DataFrame createTestDataFrame2() {
+    @BeforeEach
+    public void testDataFrameCreationArray() {
+        System.out.println("_________________Création d'un DataFrame de test_________________");
         Object[][] data = new Object[][] {
             {1, "Bonjour", true},
             {2, "Monde", false}
@@ -28,30 +37,13 @@ public class DataFrameTest {
         columns.put("A", Integer.class);
         columns.put("B", String.class);
         columns.put("C", Boolean.class);
-        return new DataFrame(data, columns);
-    }
-
-    @Test
-    public void testDataFrameCreationList() {
-        System.out.println("_________________Création d'un DataFrame de test_________________");
-        DataFrame df = createTestDataFrame();
-        assertNotEquals(null, df);
-        df.display();
-        System.out.println();
-    }
-    @Test
-    public void testDataFrameCreationArray() {
-        System.out.println("_________________Création d'un DataFrame de test_________________");
-        DataFrame df = createTestDataFrame2();
-        assertNotEquals(null, df);
-        df.display();
-        System.out.println();
+        df2 = new DataFrame(data, columns);
+        assertNotEquals(null, df2);
     }
 
     @Test
     public void testAjouterLigne() {
         System.out.println("_________________Ajout d'une ligne au DataFrame de test_________________");
-        DataFrame df = createTestDataFrame();
         List<Object> ligne = Arrays.asList(3, "Salut", true);
         df.ajouterLigne(ligne);
         System.out.println();
@@ -60,7 +52,6 @@ public class DataFrameTest {
     @Test
     public void testAjouterLigneInvalide() {
         System.out.println("_________________Ajout d'une ligne (invalide) au DataFrame de test_________________");
-        DataFrame df = createTestDataFrame();
         List<Object> ligne = Arrays.asList(3, "Salut");
         assertThrows(IllegalArgumentException.class, () -> df.ajouterLigne(ligne));
         System.out.println();
@@ -69,14 +60,12 @@ public class DataFrameTest {
     @Test
     public void testAjouterLigneInvalide2() {
         System.out.println("_________________Ajout d'une ligne (invalide) au DataFrame de test_________________");
-        DataFrame df = createTestDataFrame();
         assertThrows(IllegalArgumentException.class, () -> df.ajouterLigne(null));
         System.out.println();
     }
     @Test
     public void testSupprimerLigne() {
         System.out.println("_________________Suppression d'une ligne du DataFrame de test_________________");
-        DataFrame df = createTestDataFrame();
         df.supprimerLigne(0);
         System.out.println();
     }
@@ -84,7 +73,6 @@ public class DataFrameTest {
     @Test
     public void testSupprimerLigneInvalide() {
         System.out.println("_________________Suppression d'une ligne (invalide) du DataFrame de test_________________");
-        DataFrame df = createTestDataFrame();
         assertThrows(IllegalArgumentException.class, () -> df.supprimerLigne(2));
         System.out.println();
     }
@@ -92,7 +80,6 @@ public class DataFrameTest {
     @Test
     public void testSupprimerColonne() {
         System.out.println("_________________Suppression d'une colonne du DataFrame de test_________________");
-        DataFrame df = createTestDataFrame();
         df.supprimerColonne("B");
         System.out.println();
     }
@@ -100,7 +87,6 @@ public class DataFrameTest {
     @Test
     public void testSupprimerColonneInvalide() {
         System.out.println("_________________Suppression d'une colonne (invalide) du DataFrame de test_________________");
-        DataFrame df = createTestDataFrame();
         assertThrows(IllegalArgumentException.class, () -> df.supprimerColonne("D"));
         System.out.println();
     }
@@ -108,7 +94,6 @@ public class DataFrameTest {
     @Test
     public void testGetters() {
         System.out.println("_________________Test des getters_________________");
-        DataFrame df = createTestDataFrame();
         System.out.println(df.getData());
         System.out.println(df.getColumns());
         System.out.println();
@@ -117,7 +102,6 @@ public class DataFrameTest {
     @Test
     public void testObtenirValeur() {
         System.out.println("_________________Obtenir une valeur du DataFrame de test_________________");
-        DataFrame df = createTestDataFrame();
         String valeur = (String) df.obtenirValeur(0, "B");
         System.out.println("La valeur est : " + valeur);
         System.out.println();
@@ -126,7 +110,6 @@ public class DataFrameTest {
     @Test
     public void testObtenirValeurInvalideColonne() {
         System.out.println("_________________Obtenir une valeur (Colonne invalide) du DataFrame de test_________________");
-        DataFrame df = createTestDataFrame();
         assertThrows(IllegalArgumentException.class, () -> df.obtenirValeur(0, "D"));
         System.out.println();
     }
@@ -134,7 +117,6 @@ public class DataFrameTest {
     @Test
     public void testObtenirValeurInvalideLigne() {
         System.out.println("_________________Obtenir une valeur (Ligne invalide) du DataFrame de test_________________");
-        DataFrame df = createTestDataFrame();
         assertThrows(IllegalArgumentException.class, () -> df.obtenirValeur(10, "A"));
         System.out.println();
     }
@@ -142,7 +124,6 @@ public class DataFrameTest {
     @Test
     public void testGetIndexesColonne() {
         System.out.println("_________________Obtenir l'index d'une colonne du DataFrame de test_________________");
-        DataFrame df = createTestDataFrame();
         int index = df.getIndexesColone("B");
         System.out.println("L'index de la colonne est : " + index);
         System.out.println();
@@ -151,7 +132,6 @@ public class DataFrameTest {
     @Test
     public void testGetIndexesColonneInvalide() {
         System.out.println("_________________Obtenir l'index d'une colonne (invalide) du DataFrame de test_________________");
-        DataFrame df = createTestDataFrame();
         assertThrows(IllegalArgumentException.class, () -> df.getIndexesColone("E"));
         System.out.println();
     }
@@ -160,7 +140,6 @@ public class DataFrameTest {
     @Test
     public void testRobustesseIndexObtenirValeur() {
         System.out.println("_________________Test de robustesse (Index négatif)_________________");
-        DataFrame df = createTestDataFrame();
         assertThrows(IllegalArgumentException.class, () -> df.obtenirValeur(-1, "A"));
         System.out.println();
     }
@@ -168,7 +147,6 @@ public class DataFrameTest {
     @Test
     public void testRobustesseIndexSupprimerLigneMore() {
         System.out.println("_________________Test de robustesse (Index invalide)_________________");
-        DataFrame df = createTestDataFrame();
         assertThrows(IllegalArgumentException.class, () -> df.supprimerLigne(10));
         System.out.println();
     }
@@ -176,7 +154,6 @@ public class DataFrameTest {
     @Test
     public void testRobustesseIndexSupprimerLigneNegative() {
         System.out.println("_________________Test de robustesse (Index invalide)_________________");
-        DataFrame df = createTestDataFrame();
         assertThrows(IllegalArgumentException.class, () -> df.supprimerLigne(-1));
         System.out.println();
     }
@@ -184,7 +161,6 @@ public class DataFrameTest {
     @Test
     public void testRobustesseIndexSupprimerColonne() {
         System.out.println("_________________Test de robustesse (Index invalide)_________________");
-        DataFrame df = createTestDataFrame();
         assertThrows(IllegalArgumentException.class, () -> df.supprimerColonne("D"));
         System.out.println();
     }
