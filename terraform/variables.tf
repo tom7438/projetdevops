6f782aa4de6686397f5aae96bec5acce94faf8ff
tom7438 <tom192010@live.fr>
2024-04-17 10:42:49 +0200

    Ajout de GCloud

---
+++ terraform/variables.tf
@@ -0,0 +1,16 @@
+variable "project" {
+  default = "devops2024rtl2"
+}
+
+variable "region" {
+  default = "us-central1"
+}
+
+variable "zone" {
+  default = "us-central1-c"
+}
+
+variable "instance-name" {
+  default = "terraform-instance"
+}
+
